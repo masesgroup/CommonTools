9aba6f60e948fa26c4a5e4df4c6641c951103015
Mario Mastrodicasa <mario.mastrodicasa@masesgroup.com>
2021-01-27 01:55:32 +0100

    Added placeholder replacer

---
+++ PlaceHolderReplacer/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+*  Copyright 2021 MASES s.r.l.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Refer to LICENSE for more information.
+*/
+
+using System;
+using System.IO;
+
+namespace MASES.CommonTools.PlaceHolderReplacer
+{
+    class Program
+    {
+        static Version DeployVersion = new Version(1, 0, 0, 0);
+        const string LicensedProductVersionTemplate = "ProductCurrentVersion = ";
+        const string LicenseVersionPlaceHolder = "__VERSION_PLACEHOLDER__";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("PlaceHolderReplacer <path-to-Version.cs or product-version> <file-to-update>");
+                    Environment.Exit(1);
+                }
+
+                string searchPath = string.Empty;
+                if (!Version.TryParse(args[0], out DeployVersion))
+                {
+                    searchPath = args[0];
+                }
+
+                var destinationPath = Environment.CurrentDirectory;
+                var gitPath = Environment.CurrentDirectory;
+                destinationPath = args[1];
+
+                if (!string.IsNullOrEmpty(searchPath))
+                {
+                    var str = File.ReadAllText(searchPath);
+                    var startIndex = str.IndexOf(LicensedProductVersionTemplate);
+                    str = str.Substring(startIndex + LicensedProductVersionTemplate.Length + 1);
+                    var endIndex = str.IndexOf("\";");
+                    str = str.Substring(0, endIndex);
+                    if (!string.IsNullOrEmpty(str))
+                    {
+                        DeployVersion = new Version(str);
+                    }
+                }
+
+                var fileContent = File.ReadAllText(args[1]);
+                fileContent = fileContent.Replace(LicenseVersionPlaceHolder, DeployVersion.ToString());
+                File.WriteAllText(args[1], fileContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(1);
+            }
+        }
+    }
+}
