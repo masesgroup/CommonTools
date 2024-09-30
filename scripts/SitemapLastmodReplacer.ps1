e10817473ef6f07e2e8e5a43af779c7fd8767b2f
masesdevelopers <94312179+masesdevelopers@users.noreply.github.com>
2024-09-30 18:26:34 +0200

    Moves script from WebSitesSupport

---
+++ scripts/SitemapLastmodReplacer.ps1
@@ -0,0 +1,25 @@
+param ($baseUrl = "https://masesgroup.com", $baseFolder = ".", $inSitemapFile = "sitemap.xml", $outSitemapFile = "sitemap.xml")
+
+if ($inSitemapFile -eq $null)
+{
+    $inSitemapFile = "sitemap.xml"
+}
+
+$data = [xml](get-content -raw $inSitemapFile)
+foreach ($url in $data.urlset.url ) {
+    if ($url.loc.StartsWith($baseUrl) ) {
+        $fullPath = Join-Path $baseFolder $url.loc.Substring($baseUrl.Length)
+        if ([System.IO.File]::Exists($fullPath))
+        {
+            $path = [System.IO.Path]::GetDirectoryName($fullPath)
+            $fileName = [System.IO.Path]::GetFileName($fullPath)
+            $lastMod = @(cd $path) | Out-String
+            $lastMod = @(git log -1 --pretty="format:%cI" $fileName) | Out-String
+            if ($lastMod -ne $null)
+            {
+                $url.lastmod = $lastMod.ToString()
+            }
+        }
+    }
+}
+$data.save($outSitemapFile)
